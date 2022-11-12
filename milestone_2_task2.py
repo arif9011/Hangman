2388d5235675967932d681e59c747566889f60c4
arif9011 <115591569+arif9011@users.noreply.github.com>
2022-11-12 23:37:22 +0000

    Add files via upload

---
+++ milestone_2_task2.py
@@ -0,0 +1,6 @@
+import random
+word_list =['Apple', 'Banana', 'Orange', 'Strawberry', 'Mango']
+random.choice
+# pick a random choice from a list of strings.
+word=random.choice(word_list)
+print(word)
