394090009ee598219fab519ff3bfc30102ee710c
arif9011 <115591569+arif9011@users.noreply.github.com>
2022-11-16 17:01:29 +0000

    Update milestone_2_task4.py

--- milestone_2_task4.py
+++ milestone_2_task4.py
@@ -4,7 +4,15 @@ def check_guess(guess):
 
    # check input is valid
    if guess.isalpha() and len(guess)==1:
-        print("Good guess")
-    
-    else :
-            print(" Oops Oops! That is not a valid input.")
+
+        print("Enter is valid")
+   else:
+     print(" Oops Oops! That is not a valid input.")
+
+            #Enter is in word
+     if guess in word:
+
+                print("Good.  {guess} Enter the word")
+                
+     else:
+                print("opps! invalid guess")
