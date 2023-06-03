4d3a3a8824e0cfa3fd2f3d885deed4c434dd0773
arif9011 <115591569+arif9011@users.noreply.github.com>
2023-06-03 11:59:06 +0100

    Delete milestone_2_task2.py

--- milestone_2_task2.py
+++
@@ -1,6 +0,0 @@
-import random
-word_list =['Apple', 'Banana', 'Orange', 'Strawberry', 'Mango']
-random.choice
-# pick a random choice from a list of strings.
-word=random.choice(word_list)
-print(word)
